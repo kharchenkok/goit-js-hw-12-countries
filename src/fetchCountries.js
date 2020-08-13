5d95fffca13fcf7a1a20228b32e11ea84225f3b4
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-13 21:01:30 +0300

    fetchCountry added

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,9 @@
-function fetchCountries(){
-    // const url = 
+function fetchCountries(search) {
+  const url = `https://restcountries.eu/rest/v2/name/${search}`;
+  return fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+    });
 }
+export default fetchCountries;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './styles.css';
+import fetchCountries from './fetchCountries';
+
+const refs={
+    userInput:document.querySelector('#country-input')
+}
+console.log(refs.userInput);
+
+const searchCountries = function(e){
+    console.log(e.target.value);
+    fetchCountries(e.target.value)
+}
+
+
+refs.userInput.addEventListener('input', searchCountries)
